0e2dcbe3ea66882f6aec055ae4fa071979ae8926
AlvaroGReg <alvaro.gracia.reg@gmail.com>
2023-07-12 16:07:38 +0200

    navigation reformat

--- src/router/MainRouter.jsx
+++ src/router/MainRouter.jsx
@@ -1,125 +1,36 @@
-import React from 'react'
-import { Routes, Router, NavLink, BrowserRouter, Route, Navigate } from "react-router-dom"
-import { ToggleMenu } from '../components/nav/ToggleMenu'
-import { ToggleTheme } from '../components/nav/ToggleTheme'
-import { Home } from '../components/Home'
-import { About } from '../components/About'
-import { Web } from '../components/Web'
-import { Others } from '../components/Others'
-import { Skills } from '../components/Skills'
-import { Contact } from '../components/Contact'
-import { Error } from '../components/Error'
+import React from "react";
+import {
+	Routes,
+	Route
+} from "react-router-dom";
+import { Footer } from "../components/Footer";
+import { Nav } from "../components/nav/Nav";
 
-export const MainRouter = () => {
-  return (
-    <BrowserRouter>
+export const MainRouter = (router) => {
+  
+    return (
+      <>
+        <Nav routes={router} />
+        
+        <main>
+          <Routes>
+{/*             <Route path="/" element={router.Home} />
+            <Route path="/home" element={router.Home} />
+            <Route path="/about" element={router.} />
+            <Route path="/web" element={<Web />} />
+            <Route path="/others" element={<Others />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<Error />} />
 
-			<nav>
-                <ToggleMenu />
-				<ul>
-					<li>
-						<NavLink
-							to="home"
-							className={({ isActive }) => {
-								return isActive ? "activado" : ""
-							}}
-						>
-							Inicio
-						</NavLink>
-					</li>
-					<li>
-						<NavLink
-							to="about"
-							className={({ isActive }) => {
-								return isActive ? "activado" : ""
-							}}
-						>
-							Sobre mí
-						</NavLink>
-					</li>
-					<li>
-						<NavLink
-							to="web"
-							className={({ isActive }) => {
-								return isActive ? "activado" : ""
-							}}
-						>
-							Web
-						</NavLink>
-					</li>
-					<li>
-						<NavLink
-							to="others"
-							className={({ isActive }) => {
-								return isActive ? "activado" : ""
-							}}
-						>
-							Otros
-						</NavLink>
-					</li>
-					<li>
-						<NavLink
-							to="skills"
-							className={({ isActive }) => {
-								return isActive ? "activado" : ""
-							}}
-						>
-							Habilidades
-						</NavLink>
-					</li>
-                    <li>
-						<NavLink
-							to="contact"
-							className={({ isActive }) => {
-								return isActive ? "activado" : ""
-							}}
-						>
-							Contacto
-						</NavLink>
-					</li>
-				</ul>
-                <ToggleTheme />
-                <img src='https://alvarogreg.glitch.me/res/logo/logo-pato.png' className='navLogo' alt='AGREG Duck'></img>
-			</nav>
+            {console.log(router.Home)} */}
 
-            {/* MAIN */}
-			<main>
-				<Routes>
-					<Route
-						path="/"
-						element={<Home />}
-					/>
-					<Route
-						path="/home"
-						element={<Home />}
-					/>
-					<Route
-						path="/about"
-						element={<About />}
-					/>
-					<Route
-						path="/web"
-						element={<Web />}
-					/>
-					<Route
-						path="/others"
-						element={<Others />}
-					/>
-					<Route
-						path="/skills"
-						element={<Skills />}
-					/>
-					<Route
-						path="/contact"
-						element={<Contact />}
-					/>						
+            
+          </Routes>
+        </main>
+  
+        <Footer />
+      </>
+    )
+  }
 
-					<Route
-						path="*"
-						element={<Error />}
-					/>
-				</Routes>
-			</main>
-		</BrowserRouter>
-  )
-}
